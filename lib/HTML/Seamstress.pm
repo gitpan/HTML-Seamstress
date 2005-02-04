bdbe0f2e2097c52b12a21995580285af80885609
Terrence Brannon <metaperl@gmail.com>
2005-02-04 14:43:26 +0000

    Import of TBONE/HTML-Seamstress-2.91e from CPAN.
    gitpan-cpan-distribution: HTML-Seamstress
    gitpan-cpan-version:      2.91e
    gitpan-cpan-path:         TBONE/HTML-Seamstress-2.91e.tar.gz
    gitpan-cpan-author:       TBONE
    gitpan-cpan-maturity:     released

--- lib/HTML/Seamstress.pm
+++ lib/HTML/Seamstress.pm
@@ -12,31 +12,10 @@ use HTML::Element::Library;
 use Scalar::Listify;
 use Tie::Cycle;
 
-use base qw/HTML::TreeBuilder/;
-
-our         @ISA = qw(Exporter HTML::TreeBuilder);
-
-require Exporter;
+use base qw/HTML::TreeBuilder HTML::Element/;
 
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use HTML::Seamstress ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-
-);
-
-our $VERSION = '2.91d';
+our $VERSION = '2.91e';
 
 our $ID = 'id';
 
