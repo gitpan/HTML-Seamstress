afbb39c8f1b6e224f5da3962c0fc8534419f2bd5
Terrence Brannon <metaperl@gmail.com>
2005-02-06 02:32:01 +0000

    Import of TBONE/HTML-Seamstress-2.91f from CPAN.
    gitpan-cpan-distribution: HTML-Seamstress
    gitpan-cpan-version:      2.91f
    gitpan-cpan-path:         TBONE/HTML-Seamstress-2.91f.tar.gz
    gitpan-cpan-author:       TBONE
    gitpan-cpan-maturity:     released

--- lib/HTML/Seamstress.pm
+++ lib/HTML/Seamstress.pm
@@ -15,7 +15,7 @@ use Tie::Cycle;
 use base qw/HTML::TreeBuilder HTML::Element/;
 
 
-our $VERSION = '2.91e';
+our $VERSION = '2.91f';
 
 our $ID = 'id';
 
@@ -40,49 +40,6 @@ sub new_from_file {
   my $self = HTML::TreeBuilder->new_from_file($file);
 
   bless $self, $class;
-
-}
-
-
-sub table {
-
-  my ($s, %table) = @_;
-
-  my $table = {};
-
-  $table->{table_node} = $s->look_down(id => $table{gi_table});
-
-  my @table_gi_tr = listify $table{gi_tr} ;
-  my @iter_node = map 
-    {
-      $table->{table_node}->look_down(id => $_)
-    } @table_gi_tr;
-
-  tie $table->{iter_node}, 'Tie::Cycle', \@iter_node;
-
-  $table->{content}    = $table{content};
-  $table->{parent}     = $table->{table_node}->parent;
-
-
-  $table->{table_node}->detach;
-  $_->detach for @iter_node;
-
-  my $add_table;
-
-  while (my $row = $table{tr_data}->($table, $table{table_data})) 
-    {
-      ++$add_table;
-
-      # wont work:      my $new_iter_node = $table->{iter_node}->clone;
-      my $I = $table->{iter_node};
-      my $new_iter_node = $I->clone;
-
-
-      $table{td_data}->($new_iter_node, $row);
-      $table->{table_node}->push_content($new_iter_node);
-    }
-
-  $table->{parent}->push_content($table->{table_node}) if $add_table;
 
 }
 
